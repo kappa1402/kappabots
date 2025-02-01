2494c293f17f3a1420ed0a10f64ce5b75b111d4f
kappa1402 <xred1402@gmail.com>
2025-02-01 01:14:29 +0000

    fix

--- kappabot/mycog.py
+++ kappabot/mycog.py
@@ -57,7 +57,7 @@ class MyCog(commands.Cog):
 
         rng = random.randint(0, int(len(direwolfMoments)-1))
 
-        await ctx.send(direwolfMoments["kappa"])
+        await ctx.send(direwolfMoments[rng])
 
     @commands.command()
     async def forsen(self, ctx):
